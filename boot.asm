3670de2f16e3ba5c70f350c8722bfd21f75a6129
Linuxxx0id <vovakonki776@gmail.com>
2021-03-21 02:02:55 +0300

    started bootloader

--- boot.asm
+++ boot.asm
@@ -1,1 +1,8 @@
-.
+; Multiboot constants declaration
+MBALIGN  equ  1 << 0             ; align loaded modules on page boundaries
+MEMINFO  equ  1 << 1             ; provide memory maps
+FLAGS    equ  MBALIGN | MEMINFO  ; Multiboot 'flag' field
+MAGIC    equ  0x1BADB002         ; 'magic number' helps bootloader find the header
+CHECKSUM equ  -(MAGIC + FLAGS)   ; checksum of above, to prove multibooting
+
+
